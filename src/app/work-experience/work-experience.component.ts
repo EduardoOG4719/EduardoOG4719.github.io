a3ef83eb2ad622109ca5a2f9aadd0667c1cb5880
EduardoOG4719 <laloniuxolmedo@gmail.com>
2022-09-06 13:46:38 -0500

    create work_experience #2

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+
+  workExperience: Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+
+    let work1 = {
+      fecha: "2018-2022",
+      ubicacion: "Orizaba, Ver.",
+      puesto: "CEO",
+      empresa: "Electronic Arts",
+      logros :[
+{descripcion:"implementar un nuevo sistema de quimica para el videojuego fifa"}
+      ]
+    };
+    let work2 = {
+      fecha: "2014-2018",
+      ubicacion: "Estados Unidos",
+      puesto: "desarrollador",
+      empresa: "Epic games",
+      logros :[
+        {descripcion:"desarrollo de videojuego"}
+              ]
+    };
+    let work3 = {
+      fecha: "2010-2014",
+      ubicacion: "Orizaba, Ver",
+      puesto: "creador de paginas web",
+      empresa: "personal",
+      logros :[
+        {descripcion:"desarrollo de front-end"}
+              ]
+    };
+
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+    this.workExperience.push(work3);
+
   }
 
 }
