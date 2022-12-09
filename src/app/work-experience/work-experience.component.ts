fdd2801a0f05ca3fdd3e0355d397eacbf824f8f9
EduardoOG4719 <laloniuxolmedo@gmail.com>
2022-12-08 18:37:50 -0600

    update develop

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -6,54 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent implements OnInit {
-workExperience: Array<any>=[];
+  workExperience: Array<any>=[];
 
   constructor() { }
 
   ngOnInit(): void {
     let work1={
-fecha:"2018-2022",
-Ubicacion:"Ixtac, Ver.",
-puesto:"CEO",
-empresas:"google",
-logros:[
-{descripcion:"desarrollo de software" }
-
-]  
-
-
-
-    };
-
-    let work2={
-      fecha:"2014-2018",
+      fecha:"2018-2022",
       Ubicacion:"Orizaba, Ver.",
       puesto:"CEO",
-      empresas:"spotify",
+      empresas:"google",
       logros:[
-        {descripcion:"desarrollo de sofware" }
-        
-        ]  
+      {descripcion:"desarrollo de software" }
       
-
-
-    };
-    let work3={
-      fecha:"2010-2014",
-      Ubicacion:"Orizaba, Ver.",
-      puesto:"CEO",
-      empresas:"ADESOFT",
-      logros:[
-        {descripcion:"desarrollo de software" }
-        
-        ]  
+      ]  
       
-
-
-    };
-    this.workExperience.push(work1);
-    this.workExperience.push(work2);
-    this.workExperience.push(work3);
-  }
-
+          };
+      
+          let work2={
+            fecha:"2014-2018",
+            Ubicacion:"Estados unidos",
+            puesto:"desarrollador junior",
+            empresas:"Electronic Arts",
+            logros:[
+              {descripcion:"desarrollo de nuevo motor para videojuegos" }
+              
+              ]  
+            
+      
+      
+          };
+          let work3={
+            fecha:"2010-2014",
+            Ubicacion:"Orizaba, Ver",
+            puesto:"Dessarrollador junior",
+            empresas:"Empresa personal",
+            logros:[
+              {descripcion:"desarrollo de paginas web para grandes empresas" }
+              
+              ]  
+            
+      
+      
+          };
+          this.workExperience.push(work1);
+          this.workExperience.push(work2);
+          this.workExperience.push(work3);
+        }
 }
